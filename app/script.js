2658ec19cc1beff5e040e3290d6b3d4bf32738f1
Lasha Abuladze <lashaa.abuladzee@gmail.com>
2026-05-19 21:11:43 +0400

    style: apply custom styling to textarea component

--- app/script.js
+++ app/script.js
@@ -1,1 +1,17 @@
 `use strict`;
+
+
+const usersComment = document.querySelectorAll(`.users-comment`)
+
+
+
+
+
+
+usersComment.forEach(el => el.disabled = true);
+usersComment[0].value = `Impressive! Though it seems the drag feature could be improved. But overall it looks   . You’ve nailed the design and the responsiveness at various breakpoints works really well.`;
+
+
+console.log(usersComment[0].value.style)
+
+// console.log())
