e42c8df07b1fac8007795956eb6c0b1fa6da5e57
Lasha Abuladze <lashaa.abuladzee@gmail.com>
2026-05-26 14:57:21 +0400

    refactor: recreate comment and reply rendering logic

--- app/script.js
+++ app/script.js
@@ -241,6 +241,71 @@ const renderYourComment = function(el) {
     commentsContainer.insertAdjacentHTML(`beforeend`, yourCommentHTML);
 }
 
+const renderYourReply = function(el) {
+    const yourReplyHTML = `
+<article class="main-comment your-reply">
+
+                <div class="xx">
+                    <header class="comment-header">
+
+                        <div class="user-container">
+                            <img class="user-img avatar-img" src="${el.userImg}" alt="avatar-img">
+
+                            <span class="user-name">
+                                ${el.username}
+                            </span>
+
+                            <span class="createdAt">
+                                ${el.createdAt}
+                            </span>
+                        </div>
+
+                    </header>
+
+                    <textarea class="comment-text comment-text--main users-comment">${el.content}</textarea>
+                </div>
+
+                <div class="yy">
+                        
+                    <div class="score-container">
+                        <button class="btn--pluse btn">
+                            <svg class="icon-pluse icon--score" width="11" height="11" xmlns="http://www.w3.org/2000/svg"><path d="M6.33 10.896c.137 0 .255-.05.354-.149.1-.1.149-.217.149-.354V7.004h3.315c.136 0 .254-.05.354-.149.099-.1.148-.217.148-.354V5.272a.483.483 0 0 0-.148-.354.483.483 0 0 0-.354-.149H6.833V1.4a.483.483 0 0 0-.149-.354.483.483 0 0 0-.354-.149H4.915a.483.483 0 0 0-.354.149c-.1.1-.149.217-.149.354v3.37H1.08a.483.483 0 0 0-.354.15c-.1.099-.149.217-.149.353v1.23c0 .136.05.254.149.353.1.1.217.149.354.149h3.333v3.39c0 .136.05.254.15.353.098.1.216.149.353.149H6.33Z" fill="#C5C6EF"/>
+                            </svg>
+                        </button>
+
+                        <span class="score-number">
+                            ${el.score}
+                        </span>
+
+                        <button class="btn--minus btn">
+                            <svg class="icon-minus icon--score" width="11" height="3" xmlns="http://www.w3.org/2000/svg"><path d="M9.256 2.66c.204 0 .38-.056.53-.167.148-.11.222-.243.222-.396V.722c0-.152-.074-.284-.223-.395a.859.859 0 0 0-.53-.167H.76a.859.859 0 0 0-.53.167C.083.437.009.57.009.722v1.375c0 .153.074.285.223.396a.859.859 0 0 0 .53.167h8.495Z" fill="#C5C6EF"/>
+                            </svg>
+                        </button>
+                    </div>
+
+                    <div class="delete-edit--container">
+
+                        <button class="btn btn--delete btn--delete-edit">
+                            <svg class="icon-delete" width="12" height="14" xmlns="http://www.w3.org/2000/svg"><path d="M1.167 12.448c0 .854.7 1.552 1.555 1.552h6.222c.856 0 1.556-.698 1.556-1.552V3.5H1.167v8.948Zm10.5-11.281H8.75L7.773 0h-3.88l-.976 1.167H0v1.166h11.667V1.167Z" fill="#ED6368"/></svg>
+                            delete
+                        </button>
+                            
+
+                        <button class="btn btn--edit btn--delete-edit">
+                            <svg class="icon-edit" width="14" height="14" xmlns="http://www.w3.org/2000/svg"><path d="M13.479 2.872 11.08.474a1.75 1.75 0 0 0-2.327-.06L.879 8.287a1.75 1.75 0 0 0-.5 1.06l-.375 3.648a.875.875 0 0 0 .875.954h.078l3.65-.333c.399-.04.773-.216 1.058-.499l7.875-7.875a1.68 1.68 0 0 0-.061-2.371Zm-2.975 2.923L8.159 3.449 9.865 1.7l2.389 2.39-1.75 1.706Z" fill="#5357B6"/></svg>
+                            adit
+                        </button>
+                    </div>
+
+                </div>
+
+            </article>
+    `
+
+    replyContainer.insertAdjacentHTML(`beforeend`, yourReplyHTML);
+    replyContainer.classList.remove(`display-none`);
+}
+
 const disableTextarea = function() {
     const textareasAray = document.querySelectorAll(`.comment-text`);
     textareasAray.forEach(el => el.disabled = true);
@@ -318,16 +383,50 @@ const getdata = async function() {
 
     sendComment.addEventListener(`click`, (e) => {
         e.preventDefault();
-
+        const yourCommentText = document.querySelector(`.textarea--your-comment`);
 
         if(!replyToName) {
-            const yourCommentText = document.querySelector(`.textarea--your-comment`);
+            // const yourCommentText = document.querySelector(`.textarea--your-comment`);
             const yourComment = new CommentCl(currentUser.username, currentUser.userImg, `${yourCommentText.value}`, createdAt = `5 min ago`, score = 5);
 
             commentsArray.push(yourComment);
             renderYourComment(yourComment);
             yourCommentText.value = ``;
         }
+
+        const yourReply = new ReplyCl(currentUser.username, currentUser.userImg, `${yourCommentText.value}`, createdAt = `5 min ago`, score = 5)
+        // console.log(yourReply);
+
+        commentsArray.forEach(el => {
+            if(replyToName === el.username) {
+                const replyToUserObj = el;
+                // console.log(replyToUserObj)
+
+                el.replies.push(yourReply);
+
+                replyContainer = document.querySelector(`.${el.username}-replies`);
+                renderYourReply(yourReply);
+                
+                yourCommentText.value = ``;
+                replyToName = ``;
+            } else {
+
+                commentsArray.forEach(el => {
+
+                    if(el.replies) {
+                        el.replies.forEach(r => {
+                            // console.log(r)
+                            if(r.username === replyToName) {
+                                replyContainer = document.querySelector(`.${el.username}-replies`);
+                                renderYourReply(yourReply);
+                                yourCommentText.value = ``;
+                                replyToName = ``;
+                            }
+                        })
+                    }
+                })
+            }
+        })
     })
 
 
